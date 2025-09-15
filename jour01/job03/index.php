4a7d7a5e81de4a27341fe5f4f11401b208329531
Malo Martiniani <malo.martiniani@laplateforme.io>
2025-09-15 16:39:55 +0200

    finished

---
+++ jour01/job03/index.php
@@ -0,0 +1,38 @@
+<?php
+$var1 = "test";
+$var2 = 2;
+$var3 = true;
+$var4 = 2.5;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> <?php echo "Tableau"; ?></title>
+</head>
+<body>
+    <table>
+        <tr>    
+            <th>Variable</th>
+            <th>Type</th>
+        </tr>
+        <tr>
+            <td><?php echo $var1; ?></td>
+            <td><?php echo gettype($var1); ?></td>
+        </tr>
+        <tr>
+            <td><?php echo $var2; ?></td>
+            <td><?php echo gettype($var2); ?></td>
+        </tr>
+        <tr>
+            <td><?php echo $var3; ?></td>
+            <td><?php echo gettype($var3); ?></td>
+        </tr>
+        <tr>
+            <td><?php echo $var4; ?></td>
+            <td><?php echo gettype($var4); ?></td>
+        </tr>
+    </table>
+</body>
+</html>
