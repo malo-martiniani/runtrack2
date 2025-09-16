7bf73a7f30f8e8d38dbc770ca0aefd439c89698b
Malo Martiniani <malo.martiniani@laplateforme.io>
2025-09-16 16:52:23 +0200

    jour 05 - 06

---
+++ jour02/job05/index.php
@@ -0,0 +1,13 @@
+<?php
+for ($i = 2; $i <= 1000; $i++) {
+    $isPrime = true;
+    for ($j = 2; $j < $i; $j++) {
+        if ($i % $j == 0) {
+            $isPrime = false;
+            break;
+        }
+    }
+    if ($isPrime) {
+        echo $i . "<br>";
+    }
+}
---
+++ jour02/job07/index.php
@@ -0,0 +1,6 @@
+<?php
+$hauteur = 10;
+for ($i = 1; $i <= $hauteur; $i++) {
+    
+    
+}
